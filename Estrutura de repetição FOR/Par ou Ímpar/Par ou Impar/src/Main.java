806350b4fdff2bcccf5fa25698548621733aa157
lcostaju <luciocj_eng@outlook.com>
2024-07-09 02:00:04 -0300

    09/07/2024 01:59:17

--- Estrutura de repetição FOR/Par ou Ímpar/Par ou Impar/src/Main.java
+++ Estrutura de repetição FOR/Par ou Ímpar/Par ou Impar/src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner s = new Scanner(System.in);
+        String parImpar = "";
+        String negPos = "";
+        int n=s.nextInt();
+        
+        for(int i=0;i<n;i++){
+            
+            int x = s.nextInt();
+
+            if(x==0){
+                
+                System.out.println("NULL");
+
+            }else{
+                if(x%2==0){
+                    parImpar = "EVEN";
+                }else{
+                    parImpar = "ODD";
+                }
+    
+                if (x>0) {
+                    negPos = "POSITIVE";                
+                }else{
+                    negPos = "NEGATIVE";
+                }
+                System.out.println(parImpar+" "+negPos);
+            }
+
+            
+
+            
+        }
+        
     }
 }

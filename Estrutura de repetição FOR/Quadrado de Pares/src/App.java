1ca651c20aeb726ca78e03d4d6317966f05c52d9
lcostaju <luciocj_eng@outlook.com>
2024-07-09 01:17:47 -0300

    teste

--- Estrutura de repetição FOR/Quadrado de Pares/src/App.java
+++ Estrutura de repetição FOR/Quadrado de Pares/src/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner s = new Scanner(System.in);
 
-        // execicio1
+        // execicio1 este
         int n = 0, pow = 0;
         n = s.nextInt();
 

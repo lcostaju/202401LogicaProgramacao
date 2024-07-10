fca2cdc5b57584fe82bdc5ecffbdbd10c3ebd504
lcostaju <luciocj_eng@outlook.com>
2024-07-10 19:28:18 -0300

    maior e posição

---
+++ Estrutura de repetição FOR/Maior e Posição/src/App.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner s = new Scanner(System.in);
+
+        int n=0,maior=0,posicao=0;
+
+        for (int i = 1; i <=100; i++) {
+            
+            n=s.nextInt();
+
+            if (n>maior) {
+                maior=n;
+                posicao=i;
+            }
+
+        }
+
+        System.out.println(maior);
+        System.out.println(posicao);
+        s.close();
+    }
+}

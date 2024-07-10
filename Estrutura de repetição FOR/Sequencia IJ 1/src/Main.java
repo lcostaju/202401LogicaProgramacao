a7b7a1e9e8a496ce99c57f1ff3a6d6186b23efbc
lcostaju <luciocj_eng@outlook.com>
2024-07-10 19:44:09 -0300

    teste

--- Estrutura de repetição FOR/Sequencia IJ 1/src/Main.java
+++ Estrutura de repetição FOR/Sequencia IJ 1/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int i=1;
+        
+        for (int j = 60; j ==0 ; j = j-5) {
+            
+            System.out.println("I="+i+" "+"J="+j);
+            i+=3;
+        }
+
     }
 }
